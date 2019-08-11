18aaae3fae3da47ac6d4e98b5e3965c633c750fb
MF Softworks <it@nygmarosebeauty.com>
2019-08-11 15:50:45 +0800

    [app] implemented digital ocean spaces storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,13 +55,14 @@ return [
             'visibility' => 'public',
         ],
 
-        's3' => [
+        'spaces' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_CDN'),
         ],
 
     ],
