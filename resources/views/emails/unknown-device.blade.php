bf1a8b208969de3b2ff8e6b9b911b251df3696d3
MF Softworks <it@nygmarosebeauty.com>
2019-09-02 19:59:30 +0800

    [email] updated unknown device email template

--- resources/views/emails/unknown-device.blade.php
+++ resources/views/emails/unknown-device.blade.php
@@ -6,8 +6,8 @@
 		<span style="padding-top: 15px">
 			<h4 class="secondary"><strong>A new unknown device has signed in to your Escape account</strong></h4>
 			<br>
-			<p>Device: {{$agent->device}} {{$agent->platform}} {{$agent->browser}}</p>
-			<p>IP Address: {{$agent->ip}}</p>
+			<p>Device: {{$agent['platform']}} {{$agent['device']}} using {{$agent['browser']}}</p>
+			<p>IP Address: {{$agent['ip']}}</p>
 		</span>
 	@include('beautymail::templates.ark.contentEnd')
 
