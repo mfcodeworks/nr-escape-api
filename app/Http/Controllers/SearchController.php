695d102f48dc483f5b57fc1397e856967bba4e8f
MF Softworks <it@nygmarosebeauty.com>
2019-10-10 17:21:38 +0800

    [api] updated search controller functions

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\User;
+use App\Post;
+use App\Comment;
+use Carbon\Carbon;
+
+class SearchController extends Controller
+{
+    public function __invoke(Request $request) {
+        // Format query
+        $query = $this->formatQuery($request->input('query'));
+
+        if (isset($request->type) && $request->type == 'hashtag') {
+
+            $topPosts = Post::whereDate('created_at', '>=', now()->subDays(1)->toDateTimeString())
+                ->where('caption', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+                ->orderByRaw('(select count(*) from comments where reply_to = posts.id) desc')
+                ->orderByRaw('(select count(*) from likes where post = posts.id) desc')
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+            return response()->json([
+                'recent' => array_merge(
+                    Post::where('caption', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+                        ->latest()
+                        ->get()
+                        ->toArray(),
+                    Comment::where('text', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+                        ->latest()
+                        ->with('post')
+                        ->get()
+                        ->pluck('post')
+                        ->toArray()
+                ),
+                'top' => array_merge(
+                    $topPosts->toArray(),
+                    Comment::whereNotIn('reply_to', $topPosts->pluck('id'))
+                        ->whereDate('created_at', '>=', now()->subDays(1)->toDateTimeString())
+                        ->where('text', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+                        ->groupBy('reply_to')
+                        ->orderBy('reply_to', 'desc')
+                        ->orderByRaw('(select count(*) from likes where post = comments.reply_to) desc')
+                        ->with('post')
+                        ->get()
+                        ->pluck('post')
+                        ->toArray()
+                )
+            ]);
+        }
+
+        // Instantiate tags array
+        $tags = [];
+
+        // Find posts containing matching hashtag
+        $posts = DB::table('posts')
+            ->select('caption')
+            ->where('caption', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+            ->get()
+            ->pluck('caption')
+            ->toArray();
+
+        // Find comments containing matching hashtag
+        $comments = DB::table('comments')
+            ->select('text')
+            ->where('text', 'REGEXP', $query['hash'].'[a-zA-Z\-\_]*')
+            ->get()
+            ->pluck('text')
+            ->toArray();
+
+        // Filter tags from matches
+        foreach(array_merge($posts, $comments) as $tag) {
+            if (preg_match_all("/\B({$query['hash']}[a-zA-Z\-\_]*\b)/", $tag, $t)) {
+                $tags = array_merge($t[1], $tags);
+            }
+        }
+
+        // Find users matching query
+        $users = User::where('username', 'like', $query['user'])
+            ->limit(15)
+            ->get();
+
+        // Return match array
+        return response()->json(
+            [
+                'users' => $users,
+                'hashtags' => array_unique($tags)
+            ]
+        );
+    }
+
+    /**
+     * Format search query
+     *
+     * @param string $query
+     * @return string
+     */
+    private function formatQuery($query) {
+        if ($query[0] === '#') $query = substr($str, 1);
+        return [
+            'hash' => '#'.str_replace(' ', '%', $query),
+            'user' => '%'.str_replace(' ', '%', $query).'%'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::prefix('v1')->group(function () {
         });
 
         // resource routes
-        Route::get('search', 'ProfileController@search')->name('profile.search');
+        Route::get('search', 'SearchController')->name('profile.search');
         Route::apiResource('profile', 'ProfileController')->only(['show']);
         Route::get('profile/{id}/posts', 'ProfileController@posts')->name('profile.posts');
         Route::post('profile/{id}/block', 'BlockController@block')->name('profile.block');
